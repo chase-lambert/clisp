f081af7697ec2de304a2c04930f8248c36c17bea
Chase Lambert <chaselambert@gmail.com>
2024-09-03 15:03:57 -0400

    basic parser complete

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,5 @@
 (ns user
   #_{:clj-kondo/ignore [:use]}
   (:require 
-    [clisp.core :refer :all]
-    [clisp.core-test :refer :all])
+    [clisp.core :refer :all])
   (:use clojure.repl))
--- src/clisp/core.clj
+++ src/clisp/core.clj
@@ -1,42 +1,45 @@
 (ns clisp.core
   (:require 
-   [clojure.string :as string])
+   [clojure.string :as str])
   ;;  [clojure.edn :as edn])
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
-(defn paren-as-num [c]
-  (case c
-    "("  1
-    ")" -1
-    0))
-
-(defn num-balanced [s-of-parens]
-  (->> s-of-parens
-       (map paren-as-num)
-       (reduce +)
-       abs))
-
-(def s "(first (list 1 (+ 2 3) 9))")
+(def s "(first (list 1 (+ 2 3) 9))") 
 
 (defn tokenize [s]
   (-> s
-      (string/replace "(" " ( ")
-      (string/replace ")" " ) ")
-      (string/trim)
-      (string/split #"\s+")))
+      (str/replace "(" " ( ")
+      (str/replace ")" " ) ")
+      (str/trim)
+      (str/split #"\s+")))
 
-(defn parse-tokens [])
+(defn atomize [token]
+  (or (parse-long token) 
+      (parse-double token) 
+      (str token)))
 
-(defn parse-atom [])
+(defn parse-tokens [tokens]
+  {:pre (seq (first tokens))}
+  (let [[t & remaining] tokens]
+    (case t
+      "(" (loop [exprs []
+                 rem-tokens remaining]
+            (let [[expr new-rem-tokens] (parse-tokens rem-tokens)]
+              (if (= ")" (first new-rem-tokens))
+                [(conj exprs expr) (rest new-rem-tokens)]
+                (recur (conj exprs expr) new-rem-tokens))))
+      
+      ")" [nil remaining]
+    
+      [(atomize t) remaining])))
 
-(defn parse-list [])
+(defn parse [s]
+  (->> s            ;; "(first (list 1 (+ 2 3) 9))"
+       tokenize     ;; ["(" "first" "(" "list" "1" "(" "+" "2" "3" ")" "9" ")" ")"]
+       parse-tokens ;; [["first" ["list" 1 ["+" 2 3] 9]] ()]
+       first))
 
-(defn parse-expression [])
-
-(defn eval [])
-
-
-(defn -main [& args]
-  (println args))
+(defn -main [s]
+  (println (parse (tokenize s))))
