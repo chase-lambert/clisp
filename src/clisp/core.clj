49c3c6e7641ba90a5c7242b4e0785389ddb06c14
Chase Lambert <chaselambert@gmail.com>
2024-09-03 16:26:05 -0400

    added flowstorm for debugging

--- src/clisp/core.clj
+++ src/clisp/core.clj
@@ -6,7 +6,9 @@
 
 (set! *warn-on-reflection* true)
 
-(def s "(first (list 1 (+ 2 3) 9))") 
+(comment
+  (def s "(first (list 1 (+ 2 3) 9))") 
+  ,)
 
 (defn tokenize [s]
   (-> s
@@ -22,24 +24,30 @@
 
 (defn parse-tokens [tokens]
   {:pre (seq (first tokens))}
-  (let [[t & remaining] tokens]
-    (case t
-      "(" (loop [exprs []
+  (let [[token & remaining] tokens]
+    (case token
+      "(" (loop [parsed-exprs []
                  rem-tokens remaining]
             (let [[expr new-rem-tokens] (parse-tokens rem-tokens)]
               (if (= ")" (first new-rem-tokens))
-                [(conj exprs expr) (rest new-rem-tokens)]
-                (recur (conj exprs expr) new-rem-tokens))))
+                [(conj parsed-exprs expr) (rest new-rem-tokens)]
+                (recur (conj parsed-exprs expr) new-rem-tokens))))
       
       ")" [nil remaining]
     
-      [(atomize t) remaining])))
+      [(atomize token) remaining])))
 
 (defn parse [s]
   (->> s            ;; "(first (list 1 (+ 2 3) 9))"
        tokenize     ;; ["(" "first" "(" "list" "1" "(" "+" "2" "3" ")" "9" ")" ")"]
        parse-tokens ;; [["first" ["list" 1 ["+" 2 3] 9]] ()]
-       first))
+       first))      ;; ["first" ["list" 1 ["+" 2 3] 9]]
+
+(comment
+  (def s "(first (list 1 (+ 2 3) 9))") 
+  (parse s)
+  ,)
 
 (defn -main [s]
-  (println (parse (tokenize s))))
+  (println (parse s))) 
+
