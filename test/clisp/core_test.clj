365683e7a1d015b392f83d371660c9fbaeb42f6c
Chase Lambert <chaselambert@gmail.com>
2024-09-05 08:06:48 -0400

    added malformed code test

--- test/clisp/core_test.clj
+++ test/clisp/core_test.clj
@@ -1,14 +1,21 @@
 (ns clisp.core-test
   (:require 
     [pjstadig.humane-test-output :as humane]
-    [clojure.test :refer [deftest is testing]]
+    [clojure.test :refer :all]
     [clisp.core :refer :all]))
 
 (humane/activate!)
 
-(deftest parser-test
-  (testing "testing parser"
+(deftest good-input-parser-test
+  (testing "testing well formed source code input"
     (let [s "(first (list 1 (+ 2 3) 9))"] 
       (is (= (parse s)
-             ["first" ["list" 0 ["+" 2 3] 9]])))))
+             ["first" ["list" 1 ["+" 2 3] 9]])))))
 
+(deftest bad-input-parser-test
+  (testing "testing malformed source code input"
+    (let [s ")"] 
+      (is (thrown-with-msg? 
+            Exception 
+            #"unexpected '\)'; no malformed code please" 
+            (parse s))))))
